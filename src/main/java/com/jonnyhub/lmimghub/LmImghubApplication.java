8034cb34ad32af894f2dbcf0eb79929fca0d1f81
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-09-02 23:24:44 +0800

    init

---
+++ src/main/java/com/jonnyhub/lmimghub/LmImghubApplication.java
@@ -0,0 +1,13 @@
+package com.jonnyhub.lmimghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmImghubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmImghubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonnyhub/lmimghub/LmImghubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonnyhub.lmimghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmImghubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
